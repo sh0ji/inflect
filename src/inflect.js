1c092b2977032b2dfcf5c3e8936065723019432a
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-10-21 11:50:14 -0400

    more api-like, presets removed, and lots of optimizations

--- src/inflect.js
+++ src/inflect.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Inflect (v1.0.3): inflect.js
+ * Inflect (v1.1.0): inflect.js
  * Cleanup, modify, and save messy HTML
  * by Evan Yamanishi
  * Licensed under GPL-3.0
@@ -12,30 +12,14 @@
 // CONSTANTS
 
 const NAME = 'inflect'
-const VERSION = '1.0.3'
+const VERSION = '1.1.0'
 
 const Default = {
-    includeInDesignTasks: true,
+    autoRun: true,
+    debug: false,
+    presets: ['html-inflect-preset-indesign'],
     removeWhitespace: true,
     vocab: 'epub'
-}
-
-// tasks for common InDesign HTML export issues
-// these are run first if included via the includeInDesignTasks option
-const InDesignTasks = [{
-    'action': 'removeElement',
-    'selector': '[id^=_id],[class^=_id]'
-},
-{
-    'action': 'removeAttribute',
-    'selector': '[lang]:not(html)',
-    'attribute': 'lang'
-}]
-
-const ItemKeys = {
-    selector: 'selector',
-    tagReplacement: 'tag',
-    attributeRemoval: 'attribute'
 }
 
 const VocabAttrName = {
@@ -43,22 +27,44 @@ const VocabAttrName = {
     epub: 'epub:type'
 }
 
+const Error = {
+    ACTION_MISSING_KEY(action, missingKey) {
+        return {
+            title: `'${missingKey}' is missing for the task.`,
+            description: `The task must be in the format { action: ${action}, ${missingKey}: value }`
+        }
+    },
+    ACTION_NOT_VALID(action) {
+        return {
+            title: `${action} is not a valid action.`
+        }
+    }
+}
+
 
 // CLASS DEFINITION
 
 class Inflect {
 
-    // accepts a document, array of items (items.json), and optional config
-    constructor(doc, items, config) {
+    // accepts a document and optional config
+    constructor(doc, config) {
         this.doc = doc
         this.config = this._getConfig(config)
+        this.tasks = this._getTasks(this.config.tasks)
+        this.count = {}
 
-        if (items) {
-            this.items = this._getItems(items)
-
-            for (let item of this.items) {
-                this.inflectItem(item)
-            }
+        if (this.config.autoRun) {
+            this.tasks.map((task) => {
+                this.runTask(task, (error, taskName) => {
+                    if (error) this._handleError(error)
+                    if (taskName) {
+                        if (typeof this.count[taskName] === 'undefined') {
+                            this.count[taskName] = 0
+                        }
+                        this.count[taskName]++
+                    }
+                })
+            })
         }
     }
 
@@ -66,67 +72,112 @@ class Inflect {
     // PUBLIC
 
     // delete an element, but leave the non-empty children
-    removeElement(el) {
+    removeElement(el, task, callback) {
         while (el.firstChild) {
             el.parentNode.insertBefore(el.firstChild, el)
         }
         el.parentNode.removeChild(el)
+        callback(null, 'removeElement')
     }
 
     // delete an attribute from an element
-    removeAttr(el, item) {
-        if (item[ItemKeys.attributeRemoval] === 'all') {
-            for (let attr of el.attributes) {
-                el.removeAttribute(attr.name)
-            }
-        } else if (typeof item[ItemKeys.attributeRemoval] === 'object') {
-            for (let attr of item[ItemKeys.attributeRemoval]) {
-                el.removeAttribute(attr)
-            }
-        } else {
-            el.removeAttribute(item[ItemKeys.attributeRemoval])
+    removeAttribute(el, task, callback) {
+        switch (typeof task.attribute) {
+            case 'string':
+                if (task.attribute === 'all') {
+                    Array.from(el.attributes).map((attr) => {
+                        el.removeAttribute(attr.name)
+                    })
+                } else {
+                    el.removeAttribute(task.attribute)
+                }
+                callback(null, 'removeAttribute')
+                break;
+            case 'object':
+                task.attribute.map((attr) => {
+                    el.removeAttribute(attr)
+                })
+                callback(null, 'removeAttribute')
+                break;
+            case 'undefined':
+                callback(Error.ACTION_MISSING_KEY('removeAttribute', 'attribute'))
+                break;
+            default:
+                callback()
         }
     }
 
-    // fix element based on data in item object
-    fixElement(el, item) {
-        let currentTag = el.tagName.toLowerCase()
-        let newTag = (item[ItemKeys.tagReplacement]) ? item[ItemKeys.tagReplacement].toLowerCase() : null
-
-        // change tag by regexp replacing the opening and closing strings
-        if (newTag && currentTag !== newTag) {
+    // change tag by regexp replacing the opening and closing strings
+    replaceTag(el, task, callback) {
+        let newTag = (task.tag) ? task.tag.toLowerCase() : null
+        let currentTag = el.nodeName.toLowerCase()
+        if (!newTag) {
+            callback(Error.ACTION_MISSING_KEY('replaceTag', 'tag'))
+        } else if (currentTag !== newTag) {
             let openTag = new RegExp(`<${currentTag}\s*`, 'g')
             let closeTag = new RegExp(`/${currentTag}>`, 'g')
             el.outerHTML = el.outerHTML.replace(openTag, `<${newTag} `).replace(closeTag, `/${newTag}>`)
+            callback(null, 'replaceTag')
         }
-        this.setAttributes(el, item)
     }
 
-    // set attributes on the element based on data in item object
-    setAttributes(el, item) {
-        if (item[this.config.vocab]) {
-            el.setAttribute(VocabAttrName[this.config.vocab], item[this.config.vocab])
+    // attempt to dynamically fix the element when no action is given
+    fixElement(el, task, callback) {
+        this.replaceTag(el, task, (error, taskName) => {
+            callback(error, taskName)
+        })
+        this.setAttributes(el, task, (error, taskName) => {
+            callback(error, taskName)
+        })
+    }
+
+    // set attributes on the element based on data in task object
+    setAttributes(el, task, callback) {
+        if (task[this.config.vocab]) {
+            el.setAttribute(VocabAttrName[this.config.vocab], task[this.config.vocab])
+            callback(null, 'setAttributes')
         }
     }
 
     // the starting-point function for semantic inflection
-    inflectItem(item) {
-        let elements = this.doc.querySelectorAll(item[ItemKeys.selector])
+    runTask(task, callback) {
+        let elements = Array.from(this.doc.querySelectorAll(task.selector))
 
-        for (let el of elements) {
-            if (this.config.removeWhitespace) this._removePrecedingWhitespace(el)
+        elements.map((el) => {
+            if (this.config.removeWhitespace) {
+                this._removeWhitespace(el)
+            }
 
-            switch (item.action) {
-                case 'removeElement':
-                    this.removeElement(el)
+            switch (typeof task.action) {
+                case 'function':
+                    task.action.call(el, (error, taskName) => {
+                        callback(error, taskName)
+                    })
                     break;
-                case 'removeAttribute':
-                    this.removeAttr(el, item)
+                case 'string':
+                    this[task.action](el, task, (error, taskName) => {
+                        callback(error, taskName)
+                    })
+                    break;
+                case 'object':
+                    task.action.map((action) => {
+                        if (typeof action === 'string') {
+                            this[action](el, task, (error, taskName) => {
+                                callback(error, taskName)
+                            })
+                        } else {
+                            callback(ACTION_NOT_VALID(action))
+                        }
+                    })
+                case 'undefined':
+                    this.fixElement(el, task, (error, taskName) => {
+                        callback(error, taskName)
+                    })
                     break;
                 default:
-                    this.fixElement(el, item)
+                    callback(ACTION_NOT_VALID(task.action))
             }
-        }
+        })
     }
 
 
@@ -137,9 +188,19 @@ class Inflect {
         return Object.assign({}, Default, config)
     }
 
-    // add items to the end of default tasks
-    _getItems(items) {
-        return (this.config.includeInDesignTasks) ? InDesignTasks.concat(items) : items
+    // combine preset tasks
+    // resulting array holds all presets in order followed by user-specified tasks
+    _getTasks(tasks) {
+        tasks = (typeof tasks === 'undefined') ? [] : tasks
+        let presets = []
+        if (this.config.presets) {
+            this.config.presets.map((preset) => {
+                let p = require(preset)
+                presets = presets.concat(p)
+            })
+        }
+        tasks = presets.concat(tasks)
+        return tasks
     }
 
     // returns true if the node is an empty text string
@@ -148,18 +209,28 @@ class Inflect {
         return node && node.nodeType === 3 && node.nodeValue.trim().length === 0
     }
 
-    // remove the previous node if it's empty
-    _removePrecedingWhitespace(el) {
-        let prev = el.previousSibling
-        if (this._nodeIsEmpty(prev)) prev.remove()
+    // remove the surrounding whitespace
+    _removeWhitespace(el) {
+        let prev = el
+        while ((prev = prev.previousSibling) && this._nodeIsEmpty(prev)) {
+            prev.remove()
+        }
+        if (this._nodeIsEmpty(el.nextSibling)) el.nextSibling.remove()
     }
 
     // copy all non-empty children from one element (el) to another (newEl)
     _getChildren(el, newEl) {
-        for (let child of el.childNodes) {
+        Array.from(el.children).map((child) => {
             if (!this._nodeIsEmpty(child)) newEl.appendChild(child)
-        }
+        })
         return newEl
+    }
+
+    // needs improvement
+    _handleError(error) {
+        if (this.config.debug) {
+            console.error(error.title)
+        }
     }
 }
 
