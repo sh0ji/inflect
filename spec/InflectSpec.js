bc68e08cec408f833e669b26a853610c3763f01b
sh0ji <sh0ji@users.noreply.github.com>
2017-02-09 10:02:19 -0500

    more tests

--- spec/InflectSpec.js
+++ spec/InflectSpec.js
@@ -1,47 +1,86 @@
+'use strict';
+
+const fs = require('fs');
+const jsdom = require('jsdom');
+const Inflect = require('../index');
+const fileHtml = './spec/helpers/html.html';
+
 describe('Inflect', function() {
-    const Inflect = require('../index')
-    const fs = require('fs')
-    const jsdom = require('jsdom')
-    let inflect
+    let inflect = null;
 
     beforeEach(function() {
-        let html = fs.readFileSync('./spec/helpers/html.html', 'utf8')
-        window = jsdom.jsdom(html).defaultView
-        global.window = window
-        global.document = window.document
-        inflect = new Inflect(document)
-    })
+        let html = fs.readFileSync(fileHtml);
+        let doc = jsdom.jsdom(html);
+        inflect = new Inflect(doc);
+    });
 
-    it('should create a new inflect instance', function() {
-        expect(new Inflect(document)).toEqual(jasmine.any(Inflect))
-    })
+    it('should create a new html-inflect instance', function() {
+        expect(inflect).toEqual(jasmine.any(Inflect));
+    });
 
-    describe('get functions', function() {
-        let pkg = require('../package.json')
+    it('should contain a document object', function() {
+        expect(inflect.doc).not.toEqual(null);
+        expect(inflect.doc).not.toBeUndefined();
+        expect(inflect.doc).toEqual(jasmine.any(Object));
+        expect(inflect.doc.body).not.toBeUndefined();
+    });
+
+    describe('Getters', function() {
+        let pkg = require('../package.json');
 
         it('version should be equal to the current npm version', function() {
-            expect(inflect.version).toEqual(pkg.version)
-        })
+            expect(inflect.version).toEqual(pkg.version);
+        });
+
         it('name should be equal to the current npm name', function() {
-            expect(inflect.name).toEqual(pkg.name)
-        })
-    })
+            expect(inflect.name).toEqual(pkg.name);
+        });
+    });
 
     describe('Task runner', function() {
-        let tasks = require('./helpers/tasks.js')
+        let tasks = require('./helpers/tasks.js');
 
-        it('should change the tagName of the target', function() {
-            let task = tasks.changeTag1
-            inflect.runTask(task)
-            let target = document.querySelector(task.selector)
-            expect(target.tagName.toLowerCase()).toEqual(task.expect)
-        })
+        describe('changeTag action', function() {
+            let task = tasks.changeTag1;
+
+            beforeEach(function(done) {
+                inflect.runTask(task).then(() => done());
+            });
+
+            it('should change the tagName of the target', function() {
+                let target = inflect.doc.querySelector(task.selector);
+                expect(target.tagName.toLowerCase()).toEqual(task.expect);
+            });
+        });
 
         it('should set the role on the target', function() {
-            let task = tasks.setRole1
-            inflect.runTask(task)
-            let target = document.querySelector(task.selector)
-            expect(target.getAttribute('role')).toEqual(task.expect)
-        })
-    })
-})
+            let task = tasks.setRole1;
+            inflect.runTask(task);
+            let target = inflect.doc.querySelector(task.selector);
+            expect(target.getAttribute('role')).toEqual(task.expect);
+        });
+
+        describe('Run an array of tasks', function() {
+            let task = tasks.array1;
+
+            beforeEach(function(done) {
+                inflect.runTasks(task).then(() =>done());
+            });
+
+            it('should change content in steps', function() {
+                let target = inflect.doc.querySelector('#innertarget');
+                let pre = inflect.doc.querySelector('pre');
+                expect(/Evan is the best!/.test(target.textContent)).toEqual(true);
+                expect(Boolean(target.querySelector('.best'))).toEqual(true);
+                expect(pre.attributes.length).toEqual(0);
+            });
+
+            it('should iterate the counts', function() {
+                expect(inflect.count.changeTag).toEqual(1);
+                expect(inflect.count.evan).toEqual(2);
+                expect(inflect.count.removeAttributes).toEqual(1);
+                expect(inflect.count.setRole).toEqual(1);
+            })
+        });
+    });
+});
