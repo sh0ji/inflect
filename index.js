352a06d629f2847d2ef1025b2354891ceffc7678
sh0ji <sh0ji@users.noreply.github.com>
2017-02-09 10:01:57 -0500

    async task handling with promises

--- index.js
+++ index.js
@@ -38,45 +38,68 @@ class Inflect {
     }
 
     runTask(task) {
+        assert(task.constructor === Object, 'Task must be an object');
         assert(task.selector, 'A selector is required');
         assert(typeof task.action === 'function' ||
             typeof Inflect[task.action] === 'function',
             `${task.action} is not a valid function`);
 
-        Array.from(this.doc.querySelectorAll(task.selector)).forEach((el) => {
-            let fn = (typeof Inflect[task.action] === 'function') ?
-                Inflect[task.action] :
-                task.action;
-            fn(el, task.parameter).then((result) => {
-                result = result || fn.name;
-                this._processResults;
-            })
-        })
+        return new Promise((resolve, reject) => {
+            Array.from(this.doc.querySelectorAll(task.selector)).forEach((el) => {
+                let fn = (typeof Inflect[task.action] === 'function') ?
+                    Inflect[task.action] :
+                    task.action;
+                fn(el, task.parameter)
+                    .then((result) => {
+                        result = result || fn.name;
+                        this._processResults(result).then((res) => resolve(res));
+                    });
+            });
+        }).then((result) => this._processResults(result));
     }
 
     runTasks(tasks) {
-        for (let task of tasks) {
-            this.runTask(task);
-        }
+        return new Promise((resolve, reject) => {
+            this._async(tasks, 0).then(() => resolve());
+        });
+    }
+
+    _async(taskArr, index) {
+        return new Promise((resolve, reject) => {
+            this.runTask(taskArr[index]).then(() => {
+                index++;
+                if (index < taskArr.length) {
+                    this._async(taskArr, index).then(() => resolve());
+                } else {
+                    resolve();
+                }
+            });
+        });
     }
 
     _processResults(result) {
-        switch (result.constructor) {
-            case Array:
-                if (typeof result[1] === 'string') {
-                    this._processResults(result[0]);
-                    this._incrementCount(result[1]);
-                } else {
-                    this.runTasks(result);
-                }
-                break;
-            case Object:
-                this.runTask(result);
-                break;
-            case String:
-                this._incrementCount(result);
-                break;
-        }
+        return new Promise((resolve, reject) => {
+            if (!result) resolve();
+            switch (result.constructor) {
+                case Array:
+                    if (typeof result[1] === 'string') {
+                        this._incrementCount(result[1]);
+                        resolve(result[0]);
+                    } else {
+                        this.runTasks(result).then(() => resolve());
+                    }
+                    break;
+                case Object:
+                    this.runTask(result).then(() => resolve());
+                    break;
+                case String:
+                    this._incrementCount(result);
+                    resolve();
+                    break;
+                default:
+                    resolve();
+            }
+        });
     }
 
     _incrementCount(key) {
