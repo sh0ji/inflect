f811fd45bac04cdb89d7b5930e2fa5fbcebd88bc
Gabriel Rumbaut <grumbaut@wwnorton.com>
2020-10-06 17:06:41 -0400

    refactor(plugin-change-tag): update param handling

--- packages/plugin-change-tag/src/index.ts
+++ packages/plugin-change-tag/src/index.ts
@@ -2,18 +2,20 @@ import { Action } from '@inflect/core';
 
 export type ChangeTagOptions = { tag: HTMLElementTagNameMap };
 const changeTag: Action<Document, Element, ChangeTagOptions> = (dom, el, parameter): void => {
-	const { tag } = parameter as ChangeTagOptions;
-	const currentTag = el.tagName.toLowerCase();
-	const newTag = (tag as unknown as string).toLowerCase();
+	if (parameter) {
+		const { tag } = parameter;
+		const currentTag = el.tagName.toLowerCase();
+		const newTag = (tag as unknown as string).toLowerCase();
 
-	if (newTag && currentTag !== newTag) {
-		const parent = el.parentNode;
-		const newEl = dom.createElement(newTag);
-		Array.from(el.attributes).forEach((attr) => {
-			newEl.setAttribute(attr.name, attr.value);
-		});
-		newEl.innerHTML = el.innerHTML;
-		parent?.replaceChild(newEl, el);
+		if (newTag && currentTag !== newTag) {
+			const parent = el.parentNode;
+			const newEl = dom.createElement(newTag);
+			Array.from(el.attributes).forEach((attr) => {
+				newEl.setAttribute(attr.name, attr.value);
+			});
+			newEl.innerHTML = el.innerHTML;
+			parent?.replaceChild(newEl, el);
+		}
 	}
 };
 
