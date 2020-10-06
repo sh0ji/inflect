d7f193032c20d915fa5fdd02dc6a25658e8bc48e
Gabriel Rumbaut <grumbaut@wwnorton.com>
2020-10-06 17:07:22 -0400

    feat(plugins): add plugin-change-tag-async

--- packages/plugin-change-tag-async/src/index.ts
+++ packages/plugin-change-tag-async/src/index.ts
@@ -1,16 +1,17 @@
-import { Action } from '@inflect/core';
+import { AsyncAction } from '@inflect/core';
 import changeTag, { ChangeTagOptions } from '@inflect/plugin-change-tag';
 
-type ChangeTagAsync = Action<Document, Element, ChangeTagOptions>;
-const changeTagAsync: ChangeTagAsync = (dom, el, parameter): Promise<void> => new Promise(
-	(resolve, reject) => {
-		try {
-			changeTag(dom, el, parameter);
-			resolve();
-		} catch (err) {
-			reject(err);
-		}
-	},
-);
+const changeTagAsync: AsyncAction<Document, Element, ChangeTagOptions> = (
+	dom,
+	el,
+	parameter,
+): Promise<void> => new Promise((resolve, reject) => {
+	try {
+		changeTag(dom, el, parameter);
+		resolve();
+	} catch (err) {
+		reject(err);
+	}
+});
 
 export default changeTagAsync;
