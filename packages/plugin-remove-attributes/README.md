d165b650e295783afc4e2f4278cbef3e5c886327
Gabriel Rumbaut <grumbaut@wwnorton.com>
2020-10-06 14:48:31 -0400

    feat(plugins): add plugin-remove-attributes

---
+++ packages/plugin-remove-attributes/README.md
@@ -0,0 +1,11 @@
+# `@inflect/remove-attributes`
+
+> TODO: description
+
+## Usage
+
+```
+const removeAttributes = require('@inflect/remove-attributes');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/plugin-remove-attributes/src/index.ts
@@ -0,0 +1,21 @@
+import { Action } from '@inflect/core';
+import flattenDeep from 'lodash.flattendeep';
+
+export type RemoveAttributesOptions = { attributes: string[] } | undefined;
+
+const removeAttributes: Action<Document, Element, RemoveAttributesOptions> = (
+	_dom,
+	el,
+	parameter,
+): void => {
+	if (parameter) {
+		const { attributes } = parameter;
+		let attrs = flattenDeep(attributes.map((attr) => attr.split(/,\s*/)));
+		attrs = (attrs.includes('all'))
+			? Array.from(el.attributes).map((attr) => attr.name)
+			: attrs;
+		attrs.forEach((attr) => el.removeAttribute(attr));
+	}
+};
+
+export default removeAttributes;
