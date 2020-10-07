dce4de59b543caee45d2d961fecba5b78d05a8dd
Gabriel Rumbaut <grumbaut@wwnorton.com>
2020-10-07 14:15:49 -0400

    refactor(plugin-remove-container): update exports

--- packages/plugin-remove-container/src/index.ts
+++ packages/plugin-remove-container/src/index.ts
@@ -1,11 +1,9 @@
 import { Action } from '@inflect/core';
-import removeElement from '@inflect/plugin-remove-element';
+import { removeElement } from '@inflect/plugin-remove-element';
 
-const removeContainer: Action<Document, Element> = (
+export const removeContainer: Action<Document, Element> = (
 	dom,
 	el,
 ): void => {
 	removeElement(dom, el, { keepChildren: true });
 };
-
-export default removeContainer;
